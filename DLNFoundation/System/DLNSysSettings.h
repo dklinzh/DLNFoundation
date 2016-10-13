59c045436166d70568ae836f555375262b466485
Daniel <linzhdk@gmail.com>
2016-10-13 16:41:22 +0800

    adjustment for iOS 10

--- DLNFoundation/System/DLNSysSettings.h
+++ DLNFoundation/System/DLNSysSettings.h
@@ -9,9 +9,9 @@
 #import <Foundation/Foundation.h>
 
 NS_CLASS_AVAILABLE_IOS(8_0) @interface DLNSysSettings : NSObject
-+ (void)phoneCall:(NSString *)phoneNumber NS_AVAILABLE_IOS(8_0);
-+ (void)openAbout NS_AVAILABLE_IOS(8_0);
 + (void)openAppSelfSettings NS_AVAILABLE_IOS(8_0);
-+ (void)openWiFi NS_AVAILABLE_IOS(8_0);
-+ (void)openNotification NS_AVAILABLE_IOS(8_0);
++ (void)phoneCall:(NSString *)phoneNumber NS_AVAILABLE_IOS(8_0);
++ (void)openAbout NS_DEPRECATED_IOS(8_0, 10_0);
++ (void)openWiFi NS_DEPRECATED_IOS(8_0, 10_0);
++ (void)openNotification NS_DEPRECATED_IOS(8_0, 10_0);
 @end
